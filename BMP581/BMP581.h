a1dd69826295d59ea8b37fab4aca8511365f9f15
Ion Grozea <gvi70000@yahoo.com>
2026-04-07 21:50:32 +0200

    Add files via upload

--- BMP581/BMP581.h
+++ BMP581/BMP581.h
@@ -1,11 +1,28 @@
-/**
- * @file BMP581.h
- * @brief Header file for the BMP581 barometric pressure and temperature sensor library.
+/***************************************************************************
+ * @file [BMP581].h/.c
+ * This file contains definitions, data structures, and functions
+ * for interfacing with the ENS160 air quality sensor.
  *
- * This library contains all the definitions, enumerations, register structures, and
- * function prototypes for initializing, configuring, and reading data from the BMP581
- * sensor over I2C.
- */
+ * Copyright (c) [2024] Grozea Ion gvi70000
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ***************************************************************************/
 
 #ifndef __BMP581_H
 #define __BMP581_H
